8e480df02455cf283ad98976385ad2d646d4329c
Eael <teyeearl@gmail.com>
2023-05-15 17:39:34 +0000

    added path to use on windows

--- images/image_conv.py
+++ images/image_conv.py
@@ -1,17 +1,17 @@
-#! usr/bin/env python3
+#! /usr/bin/env python3
 
 import os, sys
 from PIL import Image
 from glob import glob 
 size = (128,128)
-
+folder = "C:/Users/teyee/Documents/PYTHON EARL/PythonAPI/images/"
 for file in glob('ic_*'): # ignore hidden file (images/.DS_Store) from iteration
     path, filename = os.path.split(file)
     filename = os.path.splitext(filename)[0] # get filename without extension
     image = Image.open(file).convert('RGB')
     image.rotate(270)
     image.resize(size)
-    image.save('/opt/icons/{}.jpeg'.format(filename))
+    image.save('C:/Users/teyee/Documents/PYTHON EARL/PythonAPI/icons/{}.jpeg'.format(filename))
 print('OK')
 
     
